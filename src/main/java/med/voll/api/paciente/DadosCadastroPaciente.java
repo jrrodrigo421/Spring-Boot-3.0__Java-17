61aa88be9783b9c7d76c23ced6a5711a82e6fed2
jrrodrigo421 <rjr89000@gmail.com>
2023-11-02 14:58:15 -0300

    feat: validations

--- src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
@@ -1,21 +1,25 @@
 package med.voll.api.paciente;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
 public record DadosCadastroPaciente(String nome,
 
-    String sobrenome,
+        @NotBlank String sobrenome,
 
-    Integer idade,
+        @NotBlank Integer idade,
 
-    String email,
+        @NotBlank @Email String email,
 
-    String cpf,
+        @NotBlank @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}") String cpf,
 
-    String telefone,
+        @NotBlank String telefone,
 
-    DadosEndereco endereco,
+        @NotBlank @Valid DadosEndereco endereco,
 
-    String motivoConsulta) {
+        @NotBlank String motivoConsulta) {
 
 }
