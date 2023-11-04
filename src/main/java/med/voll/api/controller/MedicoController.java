575bc3c1b2c12ebcf640693959d77276d7d57de5
jrrodrigo421 <rjr89000@gmail.com>
2023-11-04 14:12:52 -0300

    fea: criação rota de deletar medico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,7 +8,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -70,5 +72,10 @@ public class MedicoController {
     System.out.println("medico\n\n" + medico.getNome());
     medico.atualizarInfo(dados);
 
+  }
+
+  @DeleteMapping("/{id}")
+  public void exlcuir(@PathVariable Long id) {
+    repository.deleteById(id);
   }
 }
